2660ae1471ae3a4f8a2ac24ab548d92a6eeef987
sik1j <karkisikij2@gmail.com>
2022-04-16 22:33:04 -0700

    added static regen to index.js,[slug].js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
-export default function Recipes() {
+import { createClient } from "contentful";
+import RecipeCard from "../components/RecipeCard";
+
+export const getStaticProps = async () => {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const response = await client.getEntries({ content_type: "recipe" });
+
+  return {
+    props: { recipes: response.items },
+    revalidate: 1,
+  };
+};
+
+export default function Recipes({ recipes }) {
+  // console.log(recipes);
   return (
     <div className="recipe-list">
-      Recipe List
+      {/* {recipes.map((recipe) => {
+        return <div key={recipe.sys.id}>{recipe.fields.title}</div>;
+      })} */}
+      {recipes.map((recipe) => {
+        return <RecipeCard key={recipe.sys.id} recipe={recipe} />;
+      })}
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
     </div>
-  )
+  );
 }
