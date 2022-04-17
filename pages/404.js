5e86d60dd099dd6dcf424b3e8c8fda8e917b7bf2
sik1j <karkisikij2@gmail.com>
2022-04-17 10:36:21 -0700

    fix 404.js and add redirect to home in 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function NotFound() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  }, []);
+
   return (
     <div className="not-found">
       <h1>404</h1>
       <h2>Ooops! That page cannot be found :(</h2>
       <p>
         Redirecting to{" "}
-        <Link>
-          <a>Homepage</a> for more marmite goodness
-        </Link>
+        <Link href={"/"}>
+          <a>Homepage</a>
+        </Link>{" "}
+        for more marmite goodness
       </p>
 
       <style jsx>{`
